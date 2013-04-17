ac65596dc4598c41584e9135030dc8e23baf76db
Nathan Harris <poop@mailinator.com>
2013-04-16 21:43:49 -0400

    created login

--- BookSite/BookSite/Models/User.cs
+++ BookSite/BookSite/Models/User.cs
@@ -1,36 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
-namespace BookSite
+namespace Login.Models
 {
     public class User
     {
-        public string AccountType { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
+        [Required]
+        [Display(Name = "User name")]
         public string UserName { get; set; }
 
-        public User CreateUser(User user)
-        {
-            return user;
-        }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
 
-        public User ModifyAccount(User user)
-        {
-            return user;
-        }
+        [Display(Name = "Remember on this computer")]
+        public bool RememberMe { get; set; }
 
-        public User UserLogin(User user)
+        /// <summary>
+        /// Checks if user with given password exists in the database
+        /// </summary>
+        /// <param name="_username">User name</param>
+        /// <param name="_password">User password</param>
+        /// <returns>True if user exist and password is correct</returns>
+        public bool IsValid(string _username, string _password)
         {
-            return user;
-        }
-
-        public void UserLogout()
-        {
-
+            using (var cn = new SqlConnection(@"Data Source=jeff.cedarville.edu; Persist Security Info=True;User ID=itm3500;Password=itm3500; MultipleActiveResultSets=True"))
+            {
+                string _sql = @"SELECT [Username] FROM [VanDintel-HarrisProject_Security].[System_Users] " +
+                       @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = System.Web.Helpers.Crypto.SHA1(_password);
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+            }
         }
     }
 }
