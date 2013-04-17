155df908b7fcb2b9b401f3970395146a1e01703e
Vince <1234@zzz.com>
2013-04-16 22:37:15 -0400

    commit

---
+++ BookSite/BookSite/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Linq;
+
+namespace BookSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult FileUpload()
+        //{
+        //    DBContext dataContext = new DBContext();
+        //    var returnData = dataContext.FileDumps;
+        //    ViewData.Model = returnData.ToList();
+        //    return View(); }
+    }
+}
